12a75df8df14e4ac0c142df41eabb0a6d4a4202f
OnceWinged <OnceWinged@users.noreply.github.com>
2025-05-23 07:21:53 +0000

    deploy: cff2c825c2ba0f15b1a8e320ab6140b455efbc67

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+window.addEventListener("load", () => {
+    const input = document.getElementById("disk-track-capacity");
+    input.focus(); //default focused field
+});
 // let ioQueue = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 function runSimulation() {
     //mapping the string
